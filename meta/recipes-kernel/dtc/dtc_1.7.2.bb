rdkcentral/poky

dtc: upgrade 1.7.0 -> 1.7.2
Remove patches (both applied upstream).

Add a tests option that is disabled by default
(as previously it was inside a is_cross() condition and
so was always disabled).

(From OE-Core rev: d56ff7d80778b2afafc6882111ac5b84655ac4da)

Signed-off-by: Alexander Kanavin <alex@linutronix.de>
Signed-off-by: Mathieu Dubois-Briand <mathieu.dubois-briand@bootlin.com>
Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

meta/recipes-kernel/dtc/dtc_1.7.2.bb
@@ -10,10 +10,8 @@ LIC_FILES_CHKSUM = "file://GPL;md5=b234ee4d69f5fce4486a80fdaf4a4263 \
 
 SRC_URI = " \
     git://git.kernel.org/pub/scm/utils/dtc/dtc.git;branch=main;protocol=https \
-    file://0001-meson.build-bump-version-to-1.7.0.patch \
-    file://0002-meson-allow-building-from-shallow-clones.patch \
 "
-SRCREV = "039a99414e778332d8f9c04cbd3072e1dcc62798"
+SRCREV = "2d10aa2afe35527728db30b35ec491ecb6959e5c"
 
 UPSTREAM_CHECK_GITTAGREGEX = "v(?P<pver>\d+(\.\d+)+)"
 
@@ -24,6 +22,7 @@ inherit meson pkgconfig
 EXTRA_OEMESON = "-Dpython=disabled -Dvalgrind=disabled"
 
 PACKAGECONFIG ??= "tools"
+PACKAGECONFIG[tests] = "-Dtestss=true,-Dtests=false,"
 PACKAGECONFIG[tools] = "-Dtools=true,-Dtools=false,flex-native bison-native"
 PACKAGECONFIG[yaml] = "-Dyaml=enabled,-Dyaml=disabled,libyaml"
 

zboszor/openembedded-core

meta/recipes-kernel/dtc/dtc_1.7.2.bb
@@ -10,11 +10,9 @@ LIC_FILES_CHKSUM = "file://GPL;md5=b234ee4d69f5fce4486a80fdaf4a4263 \
 
 SRC_URI = " \
     git://git.kernel.org/pub/scm/utils/dtc/dtc.git;branch=main;protocol=https \
-    file://0001-meson.build-bump-version-to-1.7.0.patch \
-    file://0002-meson-allow-building-from-shallow-clones.patch \
     file://0001-Fix-discarded-const-qualifiers.patch \
 "
-SRCREV = "039a99414e778332d8f9c04cbd3072e1dcc62798"
+SRCREV = "2d10aa2afe35527728db30b35ec491ecb6959e5c"
 
 UPSTREAM_CHECK_GITTAGREGEX = "v(?P<pver>\d+(\.\d+)+)"
 
@@ -25,6 +23,7 @@ inherit meson pkgconfig
 EXTRA_OEMESON = "-Dpython=disabled -Dvalgrind=disabled"
 
 PACKAGECONFIG ??= "tools"
+PACKAGECONFIG[tests] = "-Dtests=true,-Dtests=false,"
 PACKAGECONFIG[tools] = "-Dtools=true,-Dtools=false,flex-native bison-native"
 PACKAGECONFIG[yaml] = "-Dyaml=enabled,-Dyaml=disabled,libyaml"
 